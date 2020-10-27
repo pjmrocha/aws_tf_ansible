45d67656ad0fb5d9ced25fae1a7f89b68309012e
pjmrocha <moi@dummy.com>
2020-10-27 20:59:34 +0000

    added variables and providers definitions

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "profile" {
+  type    = string
+  default = "tf-user"
+}
+
+variable "region-master" {
+  type    = string
+  default = "eu-west-2"
+}
+
+variable "region-worker" {
+  type    = string
+  default = "eu-west-1"
+}
